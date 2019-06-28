695e837d6e34fffa3b62c0e3fb318dfe5341c1a7
Trần Nhật Phương <nhatphuongb1@gmail.com>
2019-06-28 14:39:15 +0700

    first commit

---
+++ RabbitMQTutorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RabbitMQTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+    }
+}
